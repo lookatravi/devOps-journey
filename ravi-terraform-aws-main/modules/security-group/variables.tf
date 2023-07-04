a2954a1c214c41dd30a96f36930bd24c6c20f9d3
Ravichandran <lookatravi@outlook.com>
2023-07-04 16:31:36 +0530

    aws-terraform

---
+++ ravi-terraform-aws-main/modules/security-group/variables.tf
@@ -0,0 +1,8 @@
+variable "secgroupname" {
+  default = "ravidevops-sg-rtp"
+}
+
+variable "vpc_id" {
+   //default = "vpc-5f680722"
+   type = string
+}
---
+++ ravi-terraform-aws-main/valaxy-infra/valaxy-infra.tf
@@ -0,0 +1,48 @@
+module "myprovider"  {
+   source = "../modules/aws-provider"
+}
+
+module "vpc-module" {
+  source = "../modules/vpc"
+  subnet_id = module.my-subnet.public-subnet
+}
+
+module "my-subnet" {
+  source = "../modules/subnet"
+  vpc_id = module.vpc-module.vpc_id
+}
+module "security-group" {
+   source = "../modules/security-group"
+   vpc_id = module.vpc-module.vpc_id
+}
+module "ec2module" {
+  source = "../modules/ec2"
+  vpc_security_group_ids = ["${module.security-group.security_group_public}"]
+  subnet_id = module.my-subnet.public-subnet
+}
+
+
+output "jenkins_master_public_ip" {
+   description = "The Public IP assigned to the Jenkins master instance"
+   value = "${module.ec2module.master_public_ip}"
+}
+
+output "jenkins_slave_public_ip" {
+   description = "The Public IP assigned to the Jenkins slave instance"
+   value = "${module.ec2module.slave_public_ip}"
+}
+
+output "security_group_id" {
+   value = "${module.security-group.security_group_public}"
+}
+
+output "vpc_id" {
+  value = "${module.vpc-module.vpc_id}"
+}
+
+output "subnet_id" {
+  value = "${module.my-subnet.public-subnet}"
+}
+
+
+
