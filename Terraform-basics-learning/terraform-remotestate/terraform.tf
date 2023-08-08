74bd6cb5e106f3dd07ccaa1853538e151dd069e9
Ravichandran Sundaramurthy <59196074+lookatravi@users.noreply.github.com>
2023-08-08 22:17:04 +0530

    Update terraform.tf

--- Terraform-basics-learning/terraform-remotestate/terraform.tf
+++ Terraform-basics-learning/terraform-remotestate/terraform.tf
@@ -4,8 +4,8 @@ backend "s3" {
         bucket = "aws-ravi-demo"
         key = "aws-demo/terraform.tfstate"
         region = "us-east-1"
-        access_key = "AKIAQK2T3YYKFNF6ODPA"
-        secret_key = "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"
+        access_key = "XXXXX"
+        secret_key = "XXXX"
         dynamodb_table= "aws-ravi-demo"
 }
 }
